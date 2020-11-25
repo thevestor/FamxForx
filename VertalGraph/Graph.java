9f9013b19208d3ef763c73a2fc0d844175db5dae
FamxForx <1074862962@qq.com>
2020-11-25 19:50:22 +0800

    update graph ,add Classoptions

--- VertalGraph/Graph.java
+++ VertalGraph/Graph.java
@@ -1,5 +1,10 @@
 package VertalGraph;
 
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Graph {
 	
 	private final int MAX_VERTS=20;
@@ -27,12 +32,14 @@ public class Graph {
 		adjMat[end][start]=1;
 	}
 	public void displayVertex(int v){
-		System.out.print(vertexList[v].label + "");
+		System.out.print(vertexList[v].label + " ");
+
 	}
 	//深度优先搜索
 	public void dfs(){
 		vertexList[0].wasVisited = true;//第一个顶点标志位访问过的
 		displayVertex(0);
+
 		theStack.push(0);
 		while(!theStack.isEmpty()){
 			int v=getAdjUnvisitedVertex(theStack.peek());
@@ -46,6 +53,7 @@ public class Graph {
 		}
 		for(int j=0; j<nVerts; j++)
 			vertexList[j].wasVisited=false;//全部还原到初始状态
+		System.out.println();
 	}
 	//获取指定顶点相邻接且未被访问过的顶点
 	public int getAdjUnvisitedVertex(int v){
@@ -54,7 +62,8 @@ public class Graph {
 				return j;//找到了一个与V顶点相邻接且未被访问过的顶点
 		return -1;//未找到
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
+		long value;
 		Graph graph = new Graph();
 		graph.addVertex('A');
 		graph.addVertex('B');
@@ -66,7 +75,33 @@ public class Graph {
 		graph.addEdge(0, 3);
 		graph.addEdge(1, 3);
 		
-		System.out.print("Visits:");
-		graph.dfs();
+		while(true){
+			System.out.print("Enter the first letter of show:");
+			char choice = getChar();
+			switch (choice){
+				case 'i':
+					System.out.print("Visits:");
+					graph.dfs();
+					break;
+				default:
+					System.out.print("Invalid Entry\n");
+					break;
+			}
+		}
+	}
+	//异常处理
+	public static String getString() throws IOException{
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		String s = br.readLine();
+		return s;
+	}
+	public static char getChar() throws IOException{
+		String s = getString();
+		return s.charAt(0);
+	}
+	public static int getInt() throws IOException{
+		String s = getString();
+		return Integer.parseInt(s);
 	}
 }
